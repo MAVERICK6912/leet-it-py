a2acba0c87019a387a365103396dac537ce99c64
Umang Srivastava <Umang.Srivastava@csgi.com>
2024-12-25 22:14:05 +0530

    Updated solution to 1137

--- leetcode/1137_n-th_tribonacci_number.py
+++ leetcode/1137_n-th_tribonacci_number.py
@@ -32,9 +32,25 @@ class Solution:
         dp[n]=self.find_trib(n-3,dp)+self.find_trib(n-2,dp)+self.find_trib(n-1,dp)
         return dp[n]
 
-# DP: Top Down
-# TC: O(n)
-# SC: O(1), recursive stack + dp array
+
+# DP solution(Bottom Up)
+# TC: O(n), loop
+# SC: O(n), memory array
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n<=1:
+            return n
+        if n==2:
+            return 1
+        mem=[0]*(n+1)
+        mem[0],mem[1],mem[2]=0,1,1
+        for index in range(3,n+1):
+            mem[index]=mem[index-1]+mem[index-2]+mem[index-3]
+        return mem[n]
+
+# DP: Bottom Up with space optimization
+# TC: O(n), loop
+# SC: O(1)
 class Solution:
     def tribonacci(self, n: int) -> int:
         if n<=1:
